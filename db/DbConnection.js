c88e9a43a47a32a7e7b5b50e92c8b3d81a151cde
Aartikhatri <aartikhatri223@gmail.com>
2023-04-25 17:58:25 +0530

    fifth commit

--- db/DbConnection.js
+++ db/DbConnection.js
@@ -4,7 +4,10 @@ import mongoose from "mongoose";
 
 const DbConnection = async()=>{
     try{
-   await mongoose.connect(`${process.env.DATABASE_URL}${process.env.DATABASE_NAME}`);
+   await mongoose.connect(`${process.env.DATABASE_URL}${process.env.DATABASE_NAME}` , {
+    useUnifiedTopology: true,
+    useNewUrlParser: true
+   } );
    console.log("connected ");
     }catch(e){
      console.log(e);
